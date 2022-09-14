72419beece46781cee15f06549d6d1adaf0a8129
NailulIjlalHaq <nailul.ijlal@gmail.com>
2022-09-14 11:15:02 +0700

    first

---
+++ database/migrations/2022_09_14_011833_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id('id_posts');
+            $table->string('title');
+            $table->string('content');
+            $table->string('cover_img');
+            $table->string('tag');
+            $table->string('id_categories');
+            $table->enum('type',['pengumuman','berita','artikel']);
+            $table->boolean('is_focus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/migrations/2022_09_14_011929_create_galleries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->id('id_galleries');
+            $table->string('title');
+            $table->string('cover_img');
+            $table->string('description');
+            $table->string('id_categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+}
