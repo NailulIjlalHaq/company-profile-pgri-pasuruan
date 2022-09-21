ec3fa5dbb25d34fd0c3b0670727fa9b036c40ed2
radityaWahyu <radityaw@gmail.com>
2022-09-21 21:41:08 +0700

    Penambahan fitur pengaturan

--- resources/views/backend/master/master.blade.php
+++ resources/views/backend/master/master.blade.php
@@ -196,6 +196,9 @@
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
                                 <a href="{{ route('profil.index', 2) }}" class="nav-link {{(request()->segment(3) == '2') ? 'active' : ''}}">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Visi Misi</p>
+                                </a>
                             </li>
                             <li class="nav-item">
                                 <a href="{{ route('profil.index', 1) }}" class="nav-link {{(request()->segment(3) == '1') ? 'active' : ''}}">
