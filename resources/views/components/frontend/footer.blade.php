4c27d947450c9418413d81568beb70c72ff9d2d4
muhammadrifaldi <rifald84@gmail.com>
2022-09-26 18:27:35 +0700

    update footer

--- resources/views/components/frontend/footer.blade.php
+++ resources/views/components/frontend/footer.blade.php
@@ -1,22 +1,51 @@
 <footer class="footer">
     <div class="footer-container">
         <div class="footer__wrapper">
-            <div class="footer__wrapper--left">
+            <div class="footer__wrapper--top">
                 <div class="footer-logo-container">
-                    <div onclick="window.location.href('{{ route('feHome') }}')" class="footer__wrapper--left__logo">
+                    <div onclick="window.location.href('{{ route('feHome') }}')" class="footer-logo-container__logo">
                         <img src={{ asset('./frontend/assets/img/logo.png') }} alt="" />
                         <p>
                             PGRI <br />
                             KAB PASURUAN
                         </p>
                     </div>
-                    <div class="footer__wrapper--left__desc">
+                    <div class="footer-logo-container__desc">
                         Transform the way companies manage <br />
                         payments electronically without the high <br />
                         payment fees.
                     </div>
                 </div>
 
+                <div class="footer-address">
+                    <div class="footer-address__phone">
+                        <p>
+                            <ion-icon name="call"></ion-icon>
+                        </p>
+                        <p>
+                            <?php
+                            print_r($configs[4]['data']);
+                            ?></p>
+                    </div>
+                    <div class="footer-address__alamat">
+                        <p>
+                            <ion-icon name="location"></ion-icon>
+                        </p>
+                        <p>
+                            <?php
+                            print_r($configs[3]['data']);
+                            ?>
+                        </p>
+                    </div>
+                    <div class="footer-address__email">
+                        <p>
+                            <ion-icon name="mail"></ion-icon>
+                        </p>
+                        <p> <?php
+                        print_r($configs[5]['data']);
+                        ?></p>
+                    </div>
+                </div>
                 {{-- <div class="footer__wrapper--left__box">
                     <a href={{ route('feHome') }} class="footer__wrapper--left__box--item">Beranda</a>
                     <a href={{ route('feHome') }} class="footer__wrapper--left__box--item">Struktur</a>
@@ -29,7 +58,8 @@
                 </div> --}}
 
             </div>
-            <div class="footer__wrapper--right">
+            <hr />
+            <div class="footer__wrapper--bot">
                 <div class="footer-medsos">
                     <div class="footer-medsos--box">
                         <ion-icon onclick="window.open('')" name="logo-instagram"></ion-icon>
@@ -48,37 +78,5 @@
             </div>
         </div>
 
-        <hr />
-        <div class="footer__bottom">
-            <div class="footer-address">
-                <div class="footer-address__phone">
-                    <p>
-                        <ion-icon name="call"></ion-icon>
-                    </p>
-                    <p>
-                        <?php
-                        print_r($configs[4]['data']);
-                        ?></p>
-                </div>
-                <div class="footer-address__alamat">
-                    <p>
-                        <ion-icon name="location"></ion-icon>
-                    </p>
-                    <p>
-                        <?php
-                        print_r($configs[3]['data']);
-                        ?>
-                    </p>
-                </div>
-                <div class="footer-address__email">
-                    <p>
-                        <ion-icon name="mail"></ion-icon>
-                    </p>
-                    <p> <?php
-                    print_r($configs[5]['data']);
-                    ?></p>
-                </div>
-            </div>
-        </div>
     </div>
 </footer>
