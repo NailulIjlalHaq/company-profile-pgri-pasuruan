5567bc8198c7ff180943f10c7de72ec37a9207f8
radityaWahyu <radityaw@gmail.com>
2022-10-03 14:03:09 +0700

    update sitemap

--- resources/views/frontend/sitemap.blade.php
+++ resources/views/frontend/sitemap.blade.php
@@ -2,49 +2,49 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{route('feHome')}}</loc>
-        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',now() }}</lastmod>
+        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime(now())) }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.6</priority>
     </url>
     <url>
         <loc>{{route('feProfil',1)}}</loc>
-        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',now() }}</lastmod>
+        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime(now())) }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.6</priority>
     </url>
     <url>
         <loc>{{route('feProfil',2)}}</loc>
-        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',now() }}</lastmod>
+        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime(now())) }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.6</priority>
     </url>
     <url>
         <loc>{{route('feProfil',3)}}</loc>
-        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',now() }}</lastmod>
+        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime(now())) }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.6</priority>
     </url>
     <url>
         <loc>{{route('feProfil',4)}}</loc>
-        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',now() }}</lastmod>
+        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime(now())) }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.6</priority>
     </url>
     <url>
         <loc>{{route('feBerita')}}</loc>
-        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',now() }}</lastmod>
+        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime(now())) }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.6</priority>
     </url>
     <url>
         <loc>{{route('feArtikel')}}</loc>
-        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',now() }}</lastmod>
+        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime(now())) }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.6</priority>
     </url>
     <url>
         <loc>{{route('feAbout')}}</loc>
-        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',now() }}</lastmod>
+        <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime(now())) }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.6</priority>
     </url>
